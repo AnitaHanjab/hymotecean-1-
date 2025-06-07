0c122b9eb025a3c0ea4e486b5bc66de9a620de56
Your Mom <beannlagos@gmail.com>
2025-06-08 01:18:14 +0800

    send.php

--- send.php
+++ send.php
@@ -12,12 +12,11 @@ if (isset($_POST["send"])) {
     $mail = new PHPMailer(true);
 
     try {
-
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'lagos.p.bscs@gmail.com';
-        $mail->Password   = 'lgtotkbikfijdffi';
+        $mail->Username   = 'lagos.p.bscs@gmail.com';  
+        $mail->Password   = 'lgtotkbikfijdffi';        
         $mail->SMTPSecure = 'ssl';
         $mail->Port       = 465;
 
@@ -26,8 +25,28 @@ if (isset($_POST["send"])) {
         $mail->addReplyTo($_POST["Email"], $_POST["Name"]);
 
         $mail->isHTML(true);
-        $mail->Subject = $_POST["Projects"]; 
-        $mail->Body    = $_POST["Message"];  
+        $mail->Subject = 'New Contact Submission from ' . $_POST["Name"];
+
+        $mail->Body = '
+            <h2>Contact Request</h2>
+            <p><strong>Name:</strong> ' . htmlspecialchars($_POST["Name"]) . '</p>
+            <p><strong>Email:</strong> ' . htmlspecialchars($_POST["Email"]) . '</p>
+            <p><strong>Project Description:</strong> ' . nl2br(htmlspecialchars($_POST["Projects"])) . '</p>
+            <p><strong>Message:</strong><br>' . nl2br(htmlspecialchars($_POST["Message"])) . '</p>
+        ';
+
+        // Max size (5MB)
+        $maxFileSize = 5 * 1024 * 1024;
+
+        if (!empty($_FILES['Project']['name'][0])) {
+            foreach ($_FILES['Project']['tmp_name'] as $key => $tmp_name) {
+                if ($_FILES['Project']['error'][$key] === UPLOAD_ERR_OK) {
+                    if ($_FILES['Project']['size'][$key] <= $maxFileSize) {
+                        $mail->addAttachment($tmp_name, $_FILES['Project']['name'][$key]);
+                    }
+                }
+            }
+        }
 
         $mail->send();
         header("Location: contact.php?status=sent");
